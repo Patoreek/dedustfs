4277e5935a6be3559a8569592b3a5b7a7e2f4573
Patrick Minda <patrick@youxpowered.com.au>
2026-04-17 16:28:34 +1000

    feat: integrated skeleton for functionality to delete screenshot images.

--- src/delete_screenshots/menu.rs
+++ src/delete_screenshots/menu.rs
@@ -1,6 +1,7 @@
 use crate::theme;
 use dialoguer::Select;
 use owo_colors::OwoColorize;
+use std::io::{self, Write};
 
 const DELETE_OLDER_THAN_30_DAYS: &str = "Delete all screenshots that are older than 30 days";
 const DELETE_OLDER_THAN_60_DAYS: &str = "Delete all screenshots that are older than 60 days";
@@ -15,5 +16,33 @@ pub fn delete_screenshots_menu() {
         .items(&options)
         .interact()
         .unwrap();
-    println!("You selected: {}", options[selection]);
+
+    match options[selection] {
+        DELETE_OLDER_THAN_30_DAYS => {
+            delete_screenshots_older_than(30);
+        }
+        DELETE_OLDER_THAN_60_DAYS => {
+            delete_screenshots_older_than(60);
+        }
+        DELETE_OLDER_THAN_90_DAYS => {
+            delete_screenshots_older_than(90);
+        }
+        BACK => {
+            return;
+        }
+        _ => {
+            println!("Invalid selection");
+            return;
+        }
+    }
+}
+
+fn delete_screenshots_older_than(days: u32) {
+    println!("Deleting screenshots older than {} days...", days);
+    //TODO: Add progress bar
+    //TODO: show files that are being deleted above the progress bar
+    println!("{}","Screenshots deleted!".red().bold());
+    println!("Press Enter to continue...");
+    let _ = io::stdin().read_line(&mut String::new()).unwrap();
+    let _ = io::stdout().flush();
 }
